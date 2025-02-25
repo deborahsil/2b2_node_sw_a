c5711f96fe5b5bc0f6d813d40c5bf3ebfc807aa6
deborahsil <deborah.silva113@etec.sp.gov.br>
2025-02-25 18:19:15 -0300

    funções node

---
+++ aula2/index.js
@@ -0,0 +1,16 @@
+function soma(n1, n2){
+    return (n1 + n2)
+}
+
+function sub(n1, n2){
+    return (n1 - n2)
+}
+
+function mul(n1, n2){
+    return (n1 * n2)
+}
+
+function div(n1, n2){
+    return (n1 / n2)
+}
+console.log(`O soma deu:${(13, 63)}´)
